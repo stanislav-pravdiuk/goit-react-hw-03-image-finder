093d5532f0bd6a009e5dc76b145de44e3ba7ae1f
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-05-04 16:29:13 +0300

    fix bug

--- src/services/pixApi.js
+++ src/services/pixApi.js
@@ -1,6 +1,6 @@
 import { BASE_URL, API_KEY } from "./api";
 
-function fetchPix(searchQuery, page) {
+export default function fetchPix(searchQuery, page) {
     return fetch(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&per_page=12&page=${page}`)
                 .then(response => {
                     if (response.ok) {
@@ -10,4 +10,4 @@ function fetchPix(searchQuery, page) {
                 })
 }
 
-export default { fetchPix };
+// export default { fetchPix };
